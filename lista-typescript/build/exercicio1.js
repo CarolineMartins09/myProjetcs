c18e4bc86914622428e0974cbbcc6a196daf7ca9
carolinemartins <caroline.martins.soares09@gmail.com>
2022-12-07 16:55:11 -0300

    exercicio 1

--- lista-typescript/build/exercicio1.js
+++ lista-typescript/build/exercicio1.js
@@ -1,2 +1,10 @@
 console.log("lISTA TYPESCRIPT --- EXERCICIO 1 ---");
+const nome = process.argv[2];
+const data = process.argv[3];
+function nascimento(nome, data) {
+    data.split(",");
+    return `Olá me chamo ${nome}, nasci no dia ${data.split("/")[0]}
+     do mês de ${data.split("/")[1]} do ano de ${data.split("/")[2]}`;
+}
+console.log(nascimento(nome, data));
 //# sourceMappingURL=exercicio1.js.map
