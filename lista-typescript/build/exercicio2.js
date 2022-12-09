a3841b3e6a33e9668307d6c7ffd159a4f81aab1e
carolinemartins <caroline.martins.soares09@gmail.com>
2022-12-09 10:28:30 -0300

    exercicio 2

---
+++ lista-typescript/build/exercicio2.js
@@ -0,0 +1,7 @@
+console.log("lISTA TYPESCRIPT --- EXERCICIO 2 ---");
+function typeVariavel(variavel) {
+    console.log(typeof variavel);
+}
+typeVariavel(11);
+typeVariavel("Olá!");
+//# sourceMappingURL=exercicio2.js.map
