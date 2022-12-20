ac5ad37dc26d32f2e5d9ebb6f0c6c92cce94c8a2
carolinemartins <caroline.martins.soares09@gmail.com>
2022-12-20 20:29:08 -0300

    inicio exercicio 7

--- lista-typescript/src/exercicio7.ts
+++ lista-typescript/src/exercicio7.ts
@@ -31,3 +31,13 @@ type produto = {
     quantidade: number;
     valorUnitario: number | string;
 };
+
+const estoque: produto[] = [
+    { nome: "MacMugffin", quantidade: 37, valorUnitario: 51.04 },
+    { nome: "Vassoura voadora", quantidade: 56, valorUnitario: 210.0 },
+    { nome: "Laço da verdade", quantidade: 32, valorUnitario: 571.5 },
+    { nome: "O precioso", quantidade: 1, valorUnitario: 9181.923 },
+    { nome: "Caneta de 250 cores", quantidade: 123, valorUnitario: 17 },
+    { nome: "Plumbus", quantidade: 13, valorUnitario: 140.44 },
+    { nome: "Pokebola", quantidade: 200, valorUnitario: 99.9915 }
+];
